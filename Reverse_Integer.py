b538e9e90ed46b024ab90879c9992c3a1b658f2a
srimani1729 <97425800+srimani1729@users.noreply.github.com>
2022-05-18 13:36:33 +0530

    n=int(input()) sign=1 rev=0 if n<0:     sign=-1     n=n*(-1) while(n!=0):     rem=n%10     n//=10     rev=rev*10+rem if not -2**31<rev<2**31-1:     print("0") else:     print(sign*rev)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,15 +1,14 @@
 n=int(input())
-def reverse(n):
-    sum=0
-    sign=1
-    if(n<0):
-        sign=-1
-        n=n*-1
-    while(n>0):
-        rem=n%10
-        sum=sum*10+rem
-        n//=10
-    if not -2147483648<sum<2147483647:
-        return 0
-    return sign*sum
-print(reverse(n))
+sign=1
+rev=0
+if n<0:
+    sign=-1
+    n=n*(-1)
+while(n!=0):
+    rem=n%10
+    n//=10
+    rev=rev*10+rem
+if not -2**31<rev<2**31-1:
+    print("0")
+else:
+    print(sign*rev)
