f394e0611630948097d700257a61434e270ab697
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-02 11:47:26 +0530

    def isPrime(i):     if i==1:         return False     for j in range(2,int(i**0.5)+1):         if(i%j==0):             return False     return True
    c=0
    n=int(input())
    m=int(input())
    for i in range(n,m+1):
        if(isPrime(i)):
            c+=1
    print(c)

--- Primes_in_range.py
+++ Primes_in_range.py
@@ -1,15 +1,15 @@
-def isprime(i):
+def isPrime(i):
     if i==1:
         return False
     for j in range(2,int(i**0.5)+1):
         if(i%j==0):
             return False
     return True
-    
-m=int(input())
-n=int(input())
+
 c=0
-for i in range(m,n+1):
-    if(isprime(i)):
+n=int(input())
+m=int(input())
+for i in range(n,m+1):
+    if(isPrime(i)):
         c+=1
 print(c)
