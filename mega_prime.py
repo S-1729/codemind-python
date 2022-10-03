e13db27ef347e11c2b25da79055374b6a0639a4f
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-03 13:51:05 +0530

    def isPrime(k):     if(k==1 or k==0):         return False     for i in range(2,int(k**0.5)+1):         if(k%i==0):             return False     return True
    n=int(input())
    if(isPrime(n)):
        while(n>0):
            rem=n%10
            n//=10
            if(isPrime(rem)==False):
                print("Not Mega Prime")
                break
        else:
            print("Mega Prime")
    else:
        print("Not Mega Prime")

--- mega_prime.py
+++ mega_prime.py
@@ -5,20 +5,16 @@ def isPrime(k):
         if(k%i==0):
             return False
     return True
-
+    
 n=int(input())
-c,c1,c2=0,0,0
 if(isPrime(n)):
     while(n>0):
         rem=n%10
         n//=10
-        c+=0
         if(isPrime(rem)==False):
             print("Not Mega Prime")
             break
     else:
         print("Mega Prime")
-
-    
 else:
     print("Not Mega Prime")
