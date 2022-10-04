11b0660e975cf69ab67301570a9a9dad1253f6a1
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-04 23:53:21 +0530

    s1=input() s1=s1.lower() l=[] for i in s1:     if s1.count(i)==1:         if i not in l:             if i!=' ':                 l.append(i)
    a=sorted(l)
    for i in a:
        print(i,end="")

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -2,9 +2,11 @@ s1=input()
 s1=s1.lower()
 l=[]
 for i in s1:
-    if i not in l:
-        if i!=' ':
-            l.append(i)
+    if s1.count(i)==1:
+        if i not in l:
+            if i!=' ':
+                l.append(i)
+
 a=sorted(l)
 for i in a:
     print(i,end="")
