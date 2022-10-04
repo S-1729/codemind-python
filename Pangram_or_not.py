e36fb19e48962cbd02491e1cc9c1c95b45ba35af
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-04 22:08:19 +0530

    s=input() s=s.lower() l="abcdefghijklmnopqrstuvwxyz" for i in l:     if i not in s:         print(False)         break else:     print(True)

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,12 +1,9 @@
 s=input()
-s=s.upper()
-l=len(s)
-li=[]
-for i in range(len(s)):
-    if(s[i]!=' '):
-        if s[i] not in li:
-            li.append(s[i])
-if(len(li)==26):
-    print("True")
+s=s.lower()
+l="abcdefghijklmnopqrstuvwxyz"
+for i in l:
+    if i not in s:
+        print(False)
+        break
 else:
-    print("False")
+    print(True)
