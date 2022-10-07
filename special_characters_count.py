003e210ea227e5fa3da1ffc0f30dd8757eb781ae
Srinivas Manikanta <97425800+S-1729@users.noreply.github.com>
2022-10-07 11:03:56 +0530

    s=input() c=0 for i in s:     if(ord(i)>=65 and ord(i)<=90) or (ord(i)>=97 and ord(i)<=122) or (ord(i)>=48 and ord(i)<=57):         continue     else:         if(i != ' '):             c+=1 print(c)

--- special_characters_count.py
+++ special_characters_count.py
@@ -1,7 +1,9 @@
 s=input()
 c=0
 for i in s:
-    if(i.isalnum()==False):
-        if(i is not ' '):
+    if(ord(i)>=65 and ord(i)<=90) or (ord(i)>=97 and ord(i)<=122) or (ord(i)>=48 and ord(i)<=57):
+        continue
+    else:
+        if(i != ' '):
             c+=1
 print(c)
